0234b995c6d3d8ed4eb4f2a2e97d1499451b07b2
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-26 18:57:29 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,6 +43,7 @@ class SongsController < ApplicationController
   end
 
   def edit
+    params[:nested] = 1
     if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
