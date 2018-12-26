6de0526b032cdc7dce196ddfe8a61b553df504a6
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-26 18:58:58 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,6 +4,7 @@ module ArtistsHelper
   end
 
   def artist_select(song,nested)
+    binding.pry
     if nested.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
